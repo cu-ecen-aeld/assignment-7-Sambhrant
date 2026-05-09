1567a7f1b733014a11909effab1e0ce9fee54285
Sambhrant Chaubey <i.sambhrant.online@gmail.com>
2026-05-10 01:53:27 +0530

    added github username for assignment 7 part 2

--- misc-modules/hello.c
+++ misc-modules/hello.c
@@ -7,13 +7,13 @@ MODULE_LICENSE("Dual BSD/GPL");
 
 static int hello_init(void)
 {
-	printk(KERN_ALERT "Hello, world - I am Sambhrant\n");
+	printk(KERN_ALERT "Hello, world - I am Sambhrant cu-ecen-aeld\n");
 	return 0;
 }
 
 static void hello_exit(void)
 {
-	printk(KERN_ALERT "Goodbye, cruel world - I am Sambhrant\n");
+	printk(KERN_ALERT "Goodbye, cruel world - I am Sambhrant cu-ecen-aeld\n");
 }
 
 module_init(hello_init);
